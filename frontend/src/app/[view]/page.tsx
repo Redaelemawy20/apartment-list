b9a000a311ce6d6d69316e2db6447052ce0ffa84
Reda Elemawy <redaelemawy98@gmail.com>
2024-12-16 16:29:42 +0200

    added post man api collection

--- frontend/src/app/[view]/page.tsx
+++ frontend/src/app/[view]/page.tsx
@@ -1,17 +1,15 @@
-import Banner from '@/components/sections/Banner';
-import heroBanner from '@/assets/banner/banner@xl.webp';
 import ApartmentsCollection from '@/components/sections/ApartmentCollection';
 import { ApartmentListingType } from '../types';
 import Hero from '@/components/sections/Hero/Hero';
 
 interface Props {
-  params: { view: ApartmentListingType };
+  params: { view: string };
 }
 const ApartmentListing = ({ params: { view } }: Props) => {
   return (
     <>
       <Hero />
-      <ApartmentsCollection view={view} />
+      <ApartmentsCollection view={view as ApartmentListingType} />
     </>
   );
 };
